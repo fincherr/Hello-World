8a5d08edc205bb7c4ac1d035e02b3d8870972c74
Richard Fincher <richard.fincher@mac.com>
2016-04-16 16:08:08 -0500

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Richard Fincher on 4/16/16.
+//  Copyright © 2016 Richard Fincher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
